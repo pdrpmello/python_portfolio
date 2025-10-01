2ed6f18d149884fd3acde35354d50133c5a1aa0f
Pedro Mello <pedroh723@gmail.com>
2025-10-01 13:45:16 -0300

    Update

--- Exchange rate USD.py
+++ Exchange rate USD.py
@@ -3,11 +3,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = requests.get('https://www.remessaonline.com.br/cotacao/cotacao-dolar')
+url = requests.get('https://wise.com/br/currency-converter/dolar-hoje')
+
 soup = bs(url.text, 'html.parser')
 
-# Adjust the selector according to the actual structure of the HTML
-dollar = soup.find('div', {'class': 'style__Text-sc-1a6mtr6-2 ljisZu'}).text[:4]
+dollar = soup.find('div', {'class': '_midMarketRateAmount_14arr_139'}).text
 dollar = dollar.replace(',', '.')
-
 print(f"The current USD exchange rate to BRL is: R$ {dollar}")
+
+
+
+
